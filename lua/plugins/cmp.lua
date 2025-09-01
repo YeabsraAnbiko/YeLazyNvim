102be4ca9adc4ebfb4348ed191393e37b7f8ec23
Yeabsra <anbikoyeabsra@gmail.com>
2025-09-01 12:29:07 +0300

    deleted lazyvim.json

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -59,6 +59,16 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
         completion = {
           completeopt = "menu,menuone,noselect",
         },
