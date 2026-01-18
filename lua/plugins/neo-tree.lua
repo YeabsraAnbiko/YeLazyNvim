500db1e753116798f1aa8b246b06f44f32eaf435
Yeabsra <anbikoyeabsra@gmail.com>
2026-01-18 23:38:21 +0300

    Integrate telescope and neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,54 +9,67 @@ return {
     },
     lazy = false,
     keys = {
-      { "<leader>e", "<cmd>Neotree toggle right<cr>", desc = "Toggle NeoTree" },
+      { "<leader>e", "<cmd>Neotree toggle left<cr>", desc = "Toggle Neo-tree" },
       { "<leader>ns", "<cmd>Neotree document_symbols right<cr>", desc = "Document Symbols" },
-      { "<leader>fs", "<cmd>Neotree filesystem float<cr>", desc = "Floating Filesystem Tree" },
+      { "<leader>fs", "<cmd>Neotree filesystem float<cr>", desc = "Floating Filesystem" },
     },
+
     config = function()
+      local telescope = require("telescope.builtin")
+
+      local icons = {
+        folder_closed = "",
+        folder_open = "",
+        folder_empty = "",
+        default_file = "",
+      }
+
       require("neo-tree").setup({
         sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+
         close_if_last_window = false,
         popup_border_style = "rounded",
+
         enable_git_status = true,
         enable_diagnostics = true,
+
         open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
-        open_files_using_relative_paths = false,
-        sort_case_insensitive = false,
+
         default_component_configs = {
-          container = { enable_character_fade = true },
+          container = {
+            enable_character_fade = true,
+          },
+
           indent = {
             indent_size = 2,
             padding = 1,
             with_markers = true,
             indent_marker = "│",
             last_indent_marker = "└",
-            highlight = "NeoTreeIndentMarker",
             expander_collapsed = "",
             expander_expanded = "",
-            expander_highlight = "NeoTreeExpander",
           },
+
           icon = {
-            folder_empty_open = "",
+            folder_closed = icons.folder_closed,
+            folder_open = icons.folder_open,
+            folder_empty = icons.folder_empty,
+            folder_empty_open = icons.folder_open,
+            default = icons.default_file,
             use_filtered_colors = true,
-            folder_closed = "",
-            folder_open = "",
-            folder_empty = "",
-            default = "", --
-            highlight = "NeoTreeFileIcon",
           },
+
           modified = {
             symbol = "[+]",
-            highlight = "NeoTreeModified",
           },
+
           name = {
             trailing_slash = false,
             use_git_status_colors = true,
-            highlight = "NeoTreeFileName",
           },
+
           git_status = {
             symbols = {
-              added = "✚",
               modified = "",
               deleted = "✖",
               renamed = "󰁕",
@@ -67,91 +80,72 @@ return {
               conflict = "",
             },
           },
-          file_size = {
-            enabled = true,
-            width = 12,
-            required_width = 64,
-          },
-          type = {
-            enabled = true,
-            width = 10,
-            required_width = 122,
-          },
-          last_modified = {
-            format = "%Y-%m-%d %H:%M",
-            enabled = true,
-            width = 20,
-            required_width = 88,
-          },
-          created = {
-            format = "%Y-%m-%d %H:%M",
-            enabled = true,
-            width = 20,
-            required_width = 110,
-          },
-          symlink_target = {
-            enabled = false,
-          },
+
+          file_size = { enabled = true },
+          type = { enabled = true },
+          last_modified = { enabled = true, format = "%Y-%m-%d %H:%M" },
+          created = { enabled = true, format = "%Y-%m-%d %H:%M" },
         },
+
         window = {
           position = "left",
           width = 40,
-          mapping_options = {
-            noremap = true,
-            nowait = true,
-          },
+
           mappings = {
             ["<c-space>"] = { "toggle_node", nowait = false },
-            ["<2-LeftMouse>"] = "open",
-            ["<cr>"] = "open",
             ["<esc>"] = "cancel",
-            ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
-            ["l"] = "focus_preview",
-            ["S"] = "open_split",
-            ["s"] = "open_vsplit",
-            ["t"] = "open_tabnew",
-            ["w"] = "open_with_window_picker",
-            ["C"] = "close_node",
-            ["z"] = "close_all_nodes",
-            ["r"] = "rename",
             ["q"] = "close_window",
+            ["P"] = { "toggle_preview", config = { use_float = true } },
+
+            ["s"] = "open_vsplit",
+            ["S"] = "open_split",
+            ["t"] = "open_tabnew",
+
+            ["z"] = "close_all_nodes",
+            ["C"] = "close_node",
             ["R"] = "refresh",
-            ["?"] = "show_help",
+
             ["<"] = "prev_source",
             [">"] = "next_source",
+            ["?"] = "show_help",
           },
         },
+
         filesystem = {
+          follow_current_file = {
+            enabled = true,
+          },
+
+          hijack_netrw_behavior = "open_current",
+          use_libuv_file_watcher = true,
+
           filtered_items = {
             visible = false,
             hide_dotfiles = true,
             hide_gitignored = true,
             hide_hidden = true,
-            hide_ignored = true,
           },
-          follow_current_file = {
-            enabled = true,
-            leave_dirs_open = false,
-          },
-          group_empty_dirs = false,
-          hijack_netrw_behavior = "open_current",
-          use_libuv_file_watcher = true,
+
           window = {
             mappings = {
-              ["<bs>"] = "navigate_up",
               ["-"] = "navigate_up",
               ["u"] = "navigate_up",
-              ["."] = "set_root",
               ["H"] = "toggle_hidden",
-              ["f"] = "fuzzy_finder",
+
+              ["<leader>g"] = function()
+                telescope.live_grep({
+                  cwd = require("neo-tree.sources.manager").get_state("filesystem").path,
+                })
+              end,
+              ["f"] = function()
+                telescope.find_files({
+                  cwd = require("neo-tree.sources.manager").get_state("filesystem").path,
+                  hidden = true,
+                })
+              end,
+
               ["F"] = "fuzzy_finder_directory",
-              ["#"] = "fuzzy_sorter",
-              ["*"] = "fuzzy_sorter_directory",
               ["/"] = "filter_on_submit",
-              ["<c-x>"] = "clear_filter",
-              ["[g"] = "prev_git_modified",
-              ["]g"] = "next_git_modified",
-              ["R"] = "refresh",
               ["a"] = { "add", config = { show_path = "relative" } },
               ["d"] = "add_directory",
               ["D"] = "delete",
@@ -159,30 +153,27 @@ return {
             },
           },
         },
+
         buffers = {
           follow_current_file = {
             enabled = true,
-            leave_dirs_open = false,
           },
           group_empty_dirs = true,
           show_unloaded = true,
+
           window = {
             mappings = {
               ["D"] = "buffer_delete",
-              ["<bs>"] = "navigate_up",
-              ["u"] = "navigate_up",
               ["-"] = "navigate_up",
-              ["."] = "set_root",
-              ["R"] = "refresh",
             },
           },
         },
+
         git_status = {
           window = {
             mappings = {
-              ["a"] = "git_add_all",
-              ["gu"] = "git_unstage_file",
               ["ga"] = "git_add_file",
+              ["gu"] = "git_unstage_file",
               ["gr"] = "git_revert_file",
               ["gc"] = "git_commit",
               ["gp"] = "git_push",
@@ -190,12 +181,14 @@ return {
             },
           },
         },
+
         document_symbols = {
           follow_cursor = true,
         },
       })
     end,
   },
+
   {
     "antosha417/nvim-lsp-file-operations",
     dependencies = {
@@ -204,24 +197,6 @@ return {
     },
     config = function()
       require("lsp-file-operations").setup()
-    end,
-  },
-  {
-    "s1n7ax/nvim-window-picker",
-    event = "VeryLazy",
-    version = "2.*",
-    config = function()
-      require("window-picker").setup({
-        hint = "floating-big-letter",
-        filter_rules = {
-          include_current_win = false,
-          autoselect_one = true,
-          bo = {
-            filetype = { "neo-tree", "neo-tree-popup", "notify" },
-            buftype = { "terminal", "quickfix" },
-          },
-        },
-      })
     end,
   },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,159 +1,209 @@
 return {
   {
-    'nvim-telescope/telescope.nvim',
+    "nvim-telescope/telescope.nvim",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope-fzf-native.nvim',
-      'nvim-telescope/telescope-ui-select.nvim',
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = vim.fn.executable("make") == 1 },
+      "nvim-telescope/telescope-ui-select.nvim",
     },
-    config = function()
-      local actions = require('telescope.actions')
 
-      require('telescope').setup({
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local themes = require("telescope.themes")
+
+      telescope.setup({
         defaults = {
+          sorting_strategy = "ascending",
           prompt_prefix = "🔍 ",
           selection_caret = " ",
+          path_display = { "filename_first" },
+
           layout_strategy = "horizontal",
           layout_config = {
-            horizontal = {
-              preview_width = 0.6,
-            },
-            vertical = {
-              preview_height = 0.6,
-            },
+            prompt_position = "top",
+            horizontal = { preview_width = 0.6 },
+            vertical = { preview_height = 0.6 },
           },
-          file_ignore_patterns = { "node_modules", ".git/", "dist", "target", "%.lock" },
+
+          file_ignore_patterns = {
+            "node_modules",
+            "%.git/",
+            "dist",
+            "target",
+            "%.lock",
+          },
+
           mappings = {
             i = {
               ["<C-j>"] = actions.move_selection_next,
               ["<C-k>"] = actions.move_selection_previous,
-              ["<c-c>"] = actions.close,
-              ["<Down>"] = require('telescope.actions').cycle_history_next,
-              ["<Up>"] = require('telescope.actions').cycle_history_prev,
+              ["<C-c>"] = actions.close,
+              ["<Down>"] = actions.cycle_history_next,
+              ["<Up>"] = actions.cycle_history_prev,
             },
             n = {
               ["q"] = actions.close,
-            }
-          },
-          path_display = { "filename_first" }
-        },
-        pickers = {
-          lsp_document_symbols = {
-            layout_config = {
-              prompt_position = "top",
             },
           },
-          find_files = {
-            hidden = true,
-            layout_config = {
-              prompt_position = "top",
-            }
-          },
-          live_grep = {
-            layout_config = {
-              prompt_position = "top",
-            }
-          },
-          current_buffer_fuzzy_find = {
-            layout_config = {
-              prompt_position = "top",
-            }
-          },
-          oldfiles = {
-            layout_config = {
-              prompt_position = "top",
-            }
-          },
-          buffers = {
-            sort_lastused = true,
-            layout_config = {
-              prompt_position = "top",
-            }
-          },
-          man_pages = {
-            layout_config = {
-              prompt_position = "top",
-            }
-          },
-          colorscheme = {
-            theme = "dropdown",
-          },
-          registers = {
-            theme = "ivy",
-            layout_config = {
-              prompt_position = "top",
-            }
-          },
-          spell_suggest = {
-            theme = "cursor",
-          },
-          help_tags = {
-            layout_config = {
-              prompt_position = "top",
-            }
-          },
-          keymaps = {
-            theme = "ivy",
-          },
+        },
+
+        pickers = {
+          find_files = { hidden = true },
+          buffers = { sort_lastused = true },
+          colorscheme = themes.get_dropdown(),
+          registers = themes.get_ivy(),
+          keymaps = themes.get_ivy(),
+          vim_options = themes.get_ivy(),
+          commands = themes.get_ivy(),
+          spell_suggest = themes.get_cursor(),
           treesitter = {
-            layout_config = {
-              prompt_position = "top",
-            }
-          },
-          vim_options = {
-            theme = "ivy",
-          },
-          commands = {
-            theme = "ivy",
+            layout_config = { prompt_position = "top" },
           },
         },
+
         extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({
-              winblend = 35,
-              prompt_title = false,
-              results_title = false,
-              sorting_strategy = "ascending",
-              layout_config = {
-                width = 0.4,
-                height = 0.3,
-              },
-            })
-          },
+          ["ui-select"] = themes.get_dropdown({}),
         },
       })
-      require('telescope').load_extension('fzf')
-      require('telescope').load_extension('ui-select')
+
+      telescope.load_extension("fzf")
+      telescope.load_extension("ui-select")
     end,
+
     keys = {
-      { "<leader>ff", require('telescope.builtin').find_files,                desc = 'Find Files' },
-      { "<leader>fh", require('telescope.builtin').oldfiles,                  desc = 'Recent Files' },
-      { "<leader>fb", require('telescope.builtin').buffers,                   desc = 'Buffers' },
-      { "<leader>fg", require('telescope.builtin').live_grep,                 desc = 'Live Grep' },
-      { "<leader>ll", require('telescope.builtin').current_buffer_fuzzy_find, desc = 'Fuzzy Current Buffer' },
-      { "<leader>fr", require('telescope.builtin').resume,                    desc = 'Resume Last Search' },
-      { "<leader>ss", require('telescope.builtin').lsp_document_symbols,      desc = 'Document Symbols' },
-      { "<leader>sp", require('telescope.builtin').spell_suggest,             desc = 'Spell Suggest' },
-      { "<leader>cs", require('telescope.builtin').colorscheme,               desc = 'Colorscheme' },
-      { "<leader>rg", require('telescope.builtin').registers,                 desc = 'Registers' },
-      { "<leader>tr", require('telescope.builtin').treesitter,                desc = 'Treesitter' },
-      { "<leader>sp", require('telescope.builtin').spell_suggest,             desc = 'Spell Suggest' },
-      { "<leader>vo", require('telescope.builtin').vim_options,               desc = 'Vim Options' },
-      { "<leader>cm", require('telescope.builtin').commands,                  desc = 'Commands' },
-      { "<leader>sk", require('telescope.builtin').keymaps,                   desc = 'Search Keymaps' },
-      { "<leader>sh", require('telescope.builtin').help_tags,                 desc = 'Help Tags' },
-      { "<leader>sm", require('telescope.builtin').man_pages,                 desc = 'Man Pages' },
-      { "<leader>gs", require('telescope.builtin').grep_string,               desc = 'Search Under Cursor or Selection' },
-      { '<leader>Gf', require('telescope.builtin').git_files,                 desc = 'Git Files' },
-      { '<leader>ta', require('telescope.builtin').tags,                      desc = 'List Tags' },
+      -- Files & buffers
+      {
+        "<leader>ff",
+        function()
+          require("telescope.builtin").find_files()
+        end,
+        desc = "Find Files",
+      },
+      {
+        "<leader>fh",
+        function()
+          require("telescope.builtin").oldfiles()
+        end,
+        desc = "Recent Files",
+      },
+      {
+        "<leader>fb",
+        function()
+          require("telescope.builtin").buffers()
+        end,
+        desc = "Buffers",
+      },
+
+      -- Search
+
+      { "<leader>sp", require("telescope.builtin").spell_suggest, desc = "Spell Suggest" },
+      {
+        "<leader>fg",
+        function()
+          require("telescope.builtin").live_grep()
+        end,
+        desc = "Live Grep",
+      },
+      {
+        "<leader>gs",
+        function()
+          require("telescope.builtin").grep_string()
+        end,
+        desc = "Search Word Under Cursor",
+      },
+      {
+        "<leader>ll",
+        function()
+          require("telescope.builtin").current_buffer_fuzzy_find()
+        end,
+        desc = "Fuzzy Current Buffer",
+      },
+
+      -- LSP / code
+      {
+        "<leader>ss",
+        function()
+          require("telescope.builtin").lsp_document_symbols()
+        end,
+        desc = "Document Symbols",
+      },
+      {
+        "<leader>tr",
+        function()
+          require("telescope.builtin").treesitter()
+        end,
+        desc = "Treesitter Symbols",
+      },
+
+      -- UI / misc
+      {
+        "<leader>cs",
+        function()
+          require("telescope.builtin").colorscheme()
+        end,
+        desc = "Colorscheme",
+      },
+      {
+        "<leader>rg",
+        function()
+          require("telescope.builtin").registers()
+        end,
+        desc = "Registers",
+      },
+      {
+        "<leader>vo",
+        function()
+          require("telescope.builtin").vim_options()
+        end,
+        desc = "Vim Options",
+      },
+      {
+        "<leader>cm",
+        function()
+          require("telescope.builtin").commands()
+        end,
+        desc = "Commands",
+      },
+      {
+        "<leader>sk",
+        function()
+          require("telescope.builtin").keymaps()
+        end,
+        desc = "Keymaps",
+      },
+      {
+        "<leader>sh",
+        function()
+          require("telescope.builtin").help_tags()
+        end,
+        desc = "Help Tags",
+      },
+      {
+        "<leader>sm",
+        function()
+          require("telescope.builtin").man_pages()
+        end,
+        desc = "Man Pages",
+      },
+
+      -- Git
+      {
+        "<leader>Gf",
+        function()
+          require("telescope.builtin").git_files()
+        end,
+        desc = "Git Files",
+      },
+
+      -- Resume
+      {
+        "<leader>fr",
+        function()
+          require("telescope.builtin").resume()
+        end,
+        desc = "Resume Last Picker",
+      },
     },
   },
-  {
-    'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'make',
-    cond = vim.fn.executable("make") == 1,
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim'
-  }
 }
