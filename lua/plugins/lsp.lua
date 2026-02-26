f26c84b41a34f8f9bce07a50da8db856bc6bbdd7
Yeabsra <anbikoyeabsra@gmail.com>
2026-02-26 11:05:16 +0300

    remove lsp_line.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,11 +8,27 @@ return {
       -- nvim lsp mapings
       { "gd", vim.lsp.buf.definition, desc = "Go to Definition" },
       { "gD", vim.lsp.buf.declaration, desc = "Go to Declaration" },
+
+      {
+        "<leader>vt",
+        function()
+          local current = vim.diagnostic.config().virtual_text
+          vim.diagnostic.config({
+            virtual_text = not current,
+          })
+
+          if current then
+            vim.notify("Virtual text disabled")
+          else
+            vim.notify("Virtual text enabled")
+          end
+        end,
+        desc = "Toggle diagnostic virtual text",
+      },
     },
 
     config = function()
       vim.diagnostic.config({
-        virtual_lines = true,
         signs = true,
         underline = true,
         update_in_insert = true,
@@ -21,48 +37,6 @@ return {
           source = true,
         },
       })
-    end,
-  },
-  {
-    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-    keys = {
-      {
-        "<leader>vt",
-        function()
-          local config = vim.diagnostic.config() or {}
-          if config.virtual_text then
-            vim.diagnostic.config({ virtual_text = false, virtual_lines = true })
-          elseif config.virtual_lines then
-            vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
-          end
-        end,
-        desc = "Toggle lsp_lines",
-      },
-      {
-        "<leader>vT",
-        function()
-          local cfg = vim.diagnostic.config() or {}
-
-          local diagnostics_enabled = cfg.virtual_text or cfg.virtual_lines
-
-          if diagnostics_enabled then
-            vim.diagnostic.config({
-              virtual_text = false,
-              virtual_lines = false,
-            })
-          else
-            vim.diagnostic.config({
-              virtual_text = true,
-              virtual_lines = false,
-            })
-          end
-        end,
-        desc = "Toggle all diagnostics (virtual_text + lsp_lines)",
-      },
-    },
-    config = function()
-      require("lsp_lines").setup()
-      vim.diagnostic.config({ virtual_text = false, virtual_lines = true })
     end,
   },
   {
